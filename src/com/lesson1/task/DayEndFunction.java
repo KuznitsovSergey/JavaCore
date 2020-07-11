d748ea9af85c0f062dbbc1913dbb2602c667ec74
Sergey Kuznitsou <50965920+mandriki@users.noreply.github.com>
2020-07-11 23:37:17 +0300

    Initial commit

---
+++ src/com/lesson1/task/DayEndFunction.java
@@ -0,0 +1,125 @@
+package com.lesson1.task;
+
+import static java.lang.Math.sqrt;
+/**
+ * 1. Имеются три числа - день, месяц и год, вводимые пользователем с
+ * консоли.
+ *     Вывести дату следующего дня в формате &quot; День.Месяц.Год&quot;.
+ *     Учесть переход на следующий месяц, а также следующий год.
+ *     Форматирование строки &quot;День.Месяц.Год&quot; вынести в отдельную
+ * функцию.
+ */
+public class DayEndFunction {
+    public static void main(String[] args) {
+        // год не високосный в соответствии с григорианским календарем, год является високосным,
+        // если его номер кратен 4, но не кратен 100, а также если он кратен 400.
+        // если високосный в феврале 29 дней иначе 28
+        //1.	Январь	31
+        //2.	Февраль	28 / 29
+        //3.	Март	31
+        //4.	Апрель	30
+        //5.	Май		31
+        //6.	Июнь	30
+        //7.	Июль	31
+        //8.	Август	31
+        //9.	Сентябрь30
+        //10.	Октябрь	31
+        //11.	Ноябрь	30
+        //12.	Декабрь	31
+
+        //   int day = 29, months = 2, year = 2020;
+        nextDay(31, 12, 2020);
+
+        int a = 1, b = 2, c = 1;
+        System.out.println("дискриминант = " + discriminant(a,b,c));
+        discriminant(a, b, c);
+        isPositive(a, b, c);
+        isZero(a,b,c);
+
+    }
+    public static void nextDay(int day, int months, int year) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) && months == 2) {
+            if (day > 29) {
+                System.out.println("В этом месяце только 29 дней, введите правильную дату");
+            } else {
+                if (day == 29) {
+                    day = 0;
+                    months++;
+                }
+            }
+        } else {
+            if (day > 28 && months == 2) {
+                System.out.println("В этом месяце только 28 дней, введите правильную дату");
+            } else {
+                if (day == 28 && months == 2) {
+                    months++;
+                    day = 0;
+                }
+            }
+        }
+        if (day > 30 && (months == 4 || months == 6 || months == 9 || months == 11)) {
+            System.out.println("В этом месяце не больше 30 дней, введите правильную дату");
+        } else {
+            if (day == 30 && (months == 4 || months == 6 || months == 9 || months == 11)) {
+                months++;
+                day = 1;
+            } else {
+                if (day > 31) {
+                    System.out.println("В этом месяце не больше 31 дня, введите правильную дату");
+                } else {
+                    if (day == 31 && (months == 1 || months == 3 || months == 5
+                            || months == 7 || months == 8 || months == 10)) {
+                        months++;
+                        day = 1;
+                    } else {
+                        if (day == 31 && months == 12) {
+                            months = 1;
+                            day = 1;
+                            year++;
+                        } else {
+                            day++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(day + "." + months + "." + year);
+    }
+
+    /**
+     * 2. Написать программу, вычисляющую корни квадратного уравнения
+     * вида ax ² + bx + c = 0, где a, b и c - вводимые пользователем из консоли
+     * данные.
+     *     Учитывать только реальные корни (в случае отрицательного
+     * дискриминанта выводить сообщение пользователю).
+     *     При решении создать и использовать следующие вынесенные
+     * функции:
+     *     - функция isPositive, определяющая, является ли число
+     * положительным
+     *     - функция isZero, определяющая, является ли число нулём
+     *     - функция discriminant, вычисляющая дискриминант
+     * <p>
+     * D = b2 − 4ac
+     */
+    public static float discriminant(int a, int b, int c) {
+        int d = b * b - 4 * a * c;
+        if (d < 0){
+            System.out.println("нет корней");
+        }
+        return d;
+    }
+    public static void isPositive(int a, int b, int c) {
+        if (discriminant(a, b, c) > 0) {
+            float x1 = (float) ((-b + sqrt(discriminant(a, b, c))) / 2 * a);
+            float x2 = (float) ((-b - sqrt(discriminant(a, b, c))) / 2 * a);
+            System.out.println("x1 = " + x1);
+            System.out.println("x2 = " + x2);
+        }
+    }
+    public static void isZero(int a, int b, int c) {
+        if (discriminant(a, b, c) == 0) {
+            float x1 = (float) (-b / 2 * a);
+            System.out.println("x1 = " + x1);
+        }
+    }
+}
