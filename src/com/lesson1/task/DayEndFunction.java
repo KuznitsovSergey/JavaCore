9684ab515f06168934c97ed2eda9e00908de71d1
Sergey Kuznitsou <50965920+mandriki@users.noreply.github.com>
2020-07-13 21:46:02 +0300

    new

--- src/com/lesson1/task/DayEndFunction.java
+++ src/com/lesson1/task/DayEndFunction.java
@@ -1,6 +1,7 @@
 package com.lesson1.task;
 
 import static java.lang.Math.sqrt;
+
 /**
  * 1. Имеются три числа - день, месяц и год, вводимые пользователем с
  * консоли.
@@ -8,35 +9,40 @@ import static java.lang.Math.sqrt;
  *     Учесть переход на следующий месяц, а также следующий год.
  *     Форматирование строки &quot;День.Месяц.Год&quot; вынести в отдельную
  * функцию.
+ * год не високосный в соответствии с григорианским календарем, год является високосным,
+ * если его номер кратен 4, но не кратен 100, а также если он кратен 400.
+ * если високосный в феврале 29 дней иначе 28
+ * 1.	Январь	31
+ * 2.	Февраль	28 / 29
+ * 3.	Март	31
+ * 4.	Апрель	30
+ * 5.	Май		31
+ * 6.	Июнь	30
+ * 7.	Июль	31
+ * 8.	Август	31
+ * 9.	Сентябрь30
+ * 10.	Октябрь	31
+ * 11.	Ноябрь	30
+ * 12.	Декабрь	31
+ * <p>
+ * int day = 29, months = 2, year = 2020;
  */
 public class DayEndFunction {
     public static void main(String[] args) {
-        // год не високосный в соответствии с григорианским календарем, год является високосным,
-        // если его номер кратен 4, но не кратен 100, а также если он кратен 400.
-        // если високосный в феврале 29 дней иначе 28
-        //1.	Январь	31
-        //2.	Февраль	28 / 29
-        //3.	Март	31
-        //4.	Апрель	30
-        //5.	Май		31
-        //6.	Июнь	30
-        //7.	Июль	31
-        //8.	Август	31
-        //9.	Сентябрь30
-        //10.	Октябрь	31
-        //11.	Ноябрь	30
-        //12.	Декабрь	31
 
-        //   int day = 29, months = 2, year = 2020;
         nextDay(31, 12, 2020);
 
-        int a = 1, b = 2, c = 1;
-        System.out.println("дискриминант = " + discriminant(a,b,c));
+        int a = 1;
+        int b = 3;
+        int c = 1;
+        System.out.println("дискриминант = " + discriminant(a, b, c));
         discriminant(a, b, c);
         isPositive(a, b, c);
-        isZero(a,b,c);
+        isZero(a, b, c);
+        function(a, b, c);
 
     }
+
     public static void nextDay(int day, int months, int year) {
         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) && months == 2) {
             if (day > 29) {
@@ -102,24 +108,43 @@ public class DayEndFunction {
      * D = b2 − 4ac
      */
     public static float discriminant(int a, int b, int c) {
-        int d = b * b - 4 * a * c;
-        if (d < 0){
-            System.out.println("нет корней");
-        }
-        return d;
+        return b * b - 4 * a * c;
     }
-    public static void isPositive(int a, int b, int c) {
+
+    public static boolean isZero(int a, int b, int c) {
+        if (discriminant(a, b, c) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isPositive(int a, int b, int c) {
         if (discriminant(a, b, c) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void function(int a, int b, int c) {
+        if (isPositive(a, b, c) == true) {
             float x1 = (float) ((-b + sqrt(discriminant(a, b, c))) / 2 * a);
             float x2 = (float) ((-b - sqrt(discriminant(a, b, c))) / 2 * a);
             System.out.println("x1 = " + x1);
             System.out.println("x2 = " + x2);
-        }
-    }
-    public static void isZero(int a, int b, int c) {
-        if (discriminant(a, b, c) == 0) {
-            float x1 = (float) (-b / 2 * a);
-            System.out.println("x1 = " + x1);
+        } else {
+            if (isZero(a, b, c) == true) {
+                float x1 = (float) (-b / 2 * a);
+                System.out.println("x1 = " + x1);
+
+            } else {
+                System.out.println("нет корней");
+            }
         }
     }
 }
+
+
+
+
